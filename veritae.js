dcd85d3cda8568a11144e25b8ec8f29d46ed1e1a
Eric Martindale <eric@ericmartindale.com>
2016-08-23 04:45:22 -0700

    Add desired future.

--- veritae.js
+++ veritae.js
@@ -3,4 +3,14 @@ var config = require('./config');
 var Maki = require('maki');
 var veritae = new Maki(config);
 
+veritae.define('Service', {
+  icon: 'sitemap',
+  description: 'Products that Veritas Group offers.',
+  attributes: {
+    name: { type: String },
+    description: { type: String },
+    image: { type: String }
+  }
+});
+
 veritae.start();
